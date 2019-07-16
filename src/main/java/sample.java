5e864bde9c922073b14c9126d0fd5bd32359316c
Naruyasu Ota <Naruyasu_Oota@albus.co.jp>
2019-07-16 13:31:02 +0900

    修正

--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -2,6 +2,9 @@
 public class sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hollow World Git vvv222");
+		System.out.println("Hollow World Git 1");
+		System.out.println("Hollow World Git 2");
+		System.out.println("Hollow World Git 3");
+		System.out.println("Hollow World Git 4");
 	}
 }
